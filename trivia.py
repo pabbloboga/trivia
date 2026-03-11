fd3bf5e0d8a2fc4841ddba869cd5e37df1328900
Pablo Bogarin <pabloboga732@gmail.com>
2026-03-11 17:23:11 -0300

    se agrego codigo de trivia

--- trivia.py
+++ trivia.py
@@ -1,1 +1,13 @@
-jahamina="jaha"
+#
+
+
+name_user=input("Escribe tu nombre: ")
+print(" bienvenido al la trivia ")
+rp1=0
+punt=0
+while True:
+    rta1=print(input("ESTAMOS EN EL AÑO 2025"))
+    if rta1 == "no":
+        rp1=0
+    else:
+        rp1=rp1+1
